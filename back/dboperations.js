9835f29783d8417b838af5a78790d47016085962
sahid <sahid.enrico@gmail.com>
2021-04-11 14:07:49 -0500

    Listar Productos

--- back/dboperations.js
+++ back/dboperations.js
@@ -4,7 +4,18 @@ const sql = require('mssql')
 const axios = require('axios');
 
 
+async function getProductos() {
+    try{
+        let pool = await sql.connect(config)
+        let personas = await pool.request().query("SELECT * from productos")
+        return personas.recordsets;
+    }
+    catch(err){
+        console.log('error')
+    }
+}
+
 
 module.exports = {
-
+    getProductos: getProductos
 }
